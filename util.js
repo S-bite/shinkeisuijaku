5ab40c3ae9b997b0924e9318737b0fcdbdf61a53
S-bite <0125bitbite@gmail.com>
2020-05-16 00:50:15 +0900

    fix tweettext

--- util.js
+++ util.js
@@ -84,7 +84,7 @@ function onClick(e, gameState) {
         gameState.state = "game";
     }
     else if (gameState.state == "clear") {
-        location.href = "http://twitter.com/share?url=" + escape(location.href) + "&text=" + `神経衰弱を${miliSecToMinSec(gameState.elapsedTime)}でクリアしました！` + escape("\n" + location.href);
+        location.href = "http://twitter.com/share?url=" + escape(location.href) + "&text=" + `神経衰弱を${miliSecToMinSec(gameState.elapsedTime)}でクリアしました！`;
     }
 }
 
